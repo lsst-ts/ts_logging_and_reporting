d037378f2e7e77761a1103d45eb2792ba1230503
Steve Pothier <pothiers@gmail.com>
2024-10-28 08:06:40 -0700

    handle PARSED_NL in narrative log

--- python/lsst/ts/logging_and_reporting/parse_message.py
+++ python/lsst/ts/logging_and_reporting/parse_message.py
@@ -3,33 +3,12 @@ import re
 
 import lsst.ts.logging_and_reporting.reports as rep
 
-
-def ignore_this():  # TODO remove
-    allsrc = all.AllSources(
-        server_url="https://summit-lsp.lsst.codes",
-        min_dayobs="2024-08-02",
-        max_dayobs="2024-10-21",
-    )
-
-    eidx = [
-        idx
-        for idx, r in enumerate(allsrc.nar_src.records[:125])
-        if "Error:" in r["message_text"]
-    ]
-
-    allsrc.nar_src.records[eidx[6]]["message_text"]
-
-    re_err = re.compile(r"^.*(Error:.*)", re.DOTALL | re.DEBUG | re.MULTILINE)
-
-    re_err.match(allsrc.nar_src.records[eidx[6]]["message_text"]).group(1)
-    # => "Error: msg='Command failed', ackcmd=(ackcmd private_seqNum=1579091411, ack=&lt;SalRetCode.CMD_FAILED: -302&gt;, error=1, result='Failed: wavefrontError write(private_revCode: cdb25a59, private_sndStamp: 1725582543.3115678, private_rcvStamp: 0.0, private_seqNum: 1, private_identity: MTAOS, private_origin: 3159695, sensorId: 0, annularZernikeCoeff: 0.08561753960450617) failed: probably at least one array field is too short')\r\n"   # noqa: E501
-
-
 # Start after whitespace before error. End at string end.
 # BLACK work-around
 flags = re.DOTALL | re.ASCII | re.MULTILINE | re.IGNORECASE
 re_err = re.compile(r"(?P<err>\S*error:.*)", flags=flags)
 re_tb = re.compile(r"\b(Traceback \(most recent call last\):.*)", flags=flags)
+re_pnl = re.compile(r"&lt;PARSED_NL&gt;", flags=re.ASCII | re.MULTILINE)
 
 
 def highlight_code(matchobj):
@@ -53,6 +32,8 @@ def markup_errors(records, src_field="message_text"):
 
         # #!re.search('|'.join([re_err, re_tb])
         # #!new = re_err.sub(highlight_code, orig)
-        new = re_tb.sub(highlight_code, orig)
+
+        new = re_pnl.sub("<br>", orig)
+        new = re_tb.sub(highlight_code, new)
         if orig != new:
             r[dest_field] = new
