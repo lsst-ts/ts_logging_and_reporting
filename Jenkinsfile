4612d62d4a5c8a5c2e316065862af88e5643f2b7
Valerie Becker <29263449+Vebop@users.noreply.github.com>
2025-05-12 11:31:38 -0700

    Add conda files for packaging and update tests.

---
+++ Jenkinsfile
@@ -0,0 +1,2 @@
+@Library('JenkinsShared')_
+DevelopPipeline(name: "ts_logging_and_reporting", module_name: "lsst.ts.logging_and_reporting")
---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+import setuptools_scm
+
+setuptools.setup(
+    version=setuptools_scm.get_version(
+        write_to="python/lsst/ts/logging_and_reporting/version.py"
+    )
+)
