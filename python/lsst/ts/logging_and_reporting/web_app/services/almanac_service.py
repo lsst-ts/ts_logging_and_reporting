7b918ec59181f9b5bc17afe45e26dd5e488923ae
Eman Ali <eali@swin.edu.au>
2025-05-15 16:46:47 +1000

    get information to calculate telescope efficiency

--- python/lsst/ts/logging_and_reporting/web_app/services/almanac_service.py
+++ python/lsst/ts/logging_and_reporting/web_app/services/almanac_service.py
@@ -1,8 +1,11 @@
 import datetime
+import logging
 from lsst.ts.logging_and_reporting.almanac import Almanac
 
+logger = logging.getLogger(__name__)
+
+
 def get_almanac(dayobs_start: datetime.date, dayobs_end: datetime.date) -> list:
-    print(f"Getting almanac for start: {dayobs_start}, end: {dayobs_end}")
+    logger.info(f"Getting almanac for start: {dayobs_start}, end: {dayobs_end}")
     almanac = Almanac(min_dayobs=dayobs_start, max_dayobs=dayobs_end)
-    night_hours = almanac.night_hours
-    return night_hours
+    return almanac
