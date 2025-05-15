970737eecd496accb3da443c950e60ddb2bcbb16
Eman Ali <eali@swin.edu.au>
2025-05-15 15:39:42 +1000

    remove redundant exposure endpoint

--- python/lsst/ts/logging_and_reporting/web_app/main.py
+++ python/lsst/ts/logging_and_reporting/web_app/main.py
@@ -5,7 +5,7 @@ from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 
 from services.jira_service import get_jira_tickets
-from services.consdb_service import get_exposures, get_mock_exposures, test_my_get_exposures
+from services.consdb_service import get_mock_exposures, custom_get_exposures
 from services.almanac_service import get_almanac
 from services.narrativelog_service import get_messages
 
@@ -46,18 +46,6 @@ async def read_exposures_from_mock_data(
 
 
 @app.get("/exposures")
-async def read_exposures(
-    request: Request,
-    dayObsStart: datetime.date,
-    dayObsEnd: datetime.date,
-    instrument: str):
-    logger.info(f"Getting exposures from adapter for start: "
-                f"{dayObsStart}, end: {dayObsEnd} "
-                f"and instrument: {instrument}")
-    exposures = get_exposures(dayObsStart, dayObsEnd, instrument)
-    return {"exposures_count": len(exposures)}
-
-@app.get("/test-exposures")
 async def test_read_exposures(
     request: Request,
     dayObsStart: datetime.date,
@@ -66,8 +54,8 @@ async def test_read_exposures(
     logger.info(f"Testing exposures from adapter custom func for start: "
                 f"{dayObsStart}, end: {dayObsEnd} "
                 f"and instrument: {instrument}")
-    exposures = test_my_get_exposures(dayObsStart, dayObsEnd, instrument)
-    return {"exposures": exposures}
+    exposures = custom_get_exposures(dayObsStart, dayObsEnd, instrument)
+    return {"exposures_count": len(exposures)}
 
 
 @app.get("/jira-tickets")
