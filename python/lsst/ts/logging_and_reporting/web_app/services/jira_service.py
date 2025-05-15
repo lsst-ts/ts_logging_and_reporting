31258ebff9272d149cdc8440d1741334bf843be1
Eman Ali <eali@swin.edu.au>
2025-05-15 13:16:03 +1000

    move the web app dir to be under lsst/ts/logging_and_reporting

--- python/lsst/ts/logging_and_reporting/web_app/services/jira_service.py
+++ python/lsst/ts/logging_and_reporting/web_app/services/jira_service.py
@@ -25,9 +25,15 @@ def extract_ticket_info(tic) -> JiraTicket:
     }
 
 
-def get_jira_tickets(dayobs_start: datetime.date, dayobs_end: datetime.date, telescope: str) -> list[JiraTicket]:
-    print(f"Getting Jira tickets for start: {dayobs_start}, end: {dayobs_end} and telescope: {telescope}")
-    # TODO: replace with code to retrieve ticket information using data adaptors
+def get_jira_tickets(
+        dayobs_start: datetime.date,
+        dayobs_end: datetime.date,
+        telescope: str
+        ) -> list[JiraTicket]:
+    print(f"Getting Jira tickets for start: {dayobs_start}, "
+          f"end: {dayobs_end} and telescope: {telescope}")
+    # TODO: replace with code to retrieve
+    # ticket information using data adaptors
     with open("data/jira-tickets.json") as f:
         content = json.load(f)
         tickets = [{
