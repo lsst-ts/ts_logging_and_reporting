b240a75fdf1f049313492ec6e0240c09148ec6ee
alserene <aserene@swin.edu.au>
2025-06-02 21:48:15 +0930

    Query fields for Exposure Breakdown applet.

--- python/lsst/ts/logging_and_reporting/web_app/services/consdb_service.py
+++ python/lsst/ts/logging_and_reporting/web_app/services/consdb_service.py
@@ -61,7 +61,7 @@ def get_exposures(
             f"min_dayobs: {cons_db.min_dayobs}, "
             f"telescope: {telescope}"
         )
-        ssql = f"""SELECT exposure_id, exp_time, img_type
+        ssql = f"""SELECT exposure_id, exp_time, img_type, observation_reason, science_program, target_name
           FROM cdb_{telescope}.exposure e
           WHERE {nd_utils.dayobs_int(cons_db.min_dayobs)} <= e.day_obs
               AND e.day_obs < {nd_utils.dayobs_int(cons_db.max_dayobs)}
