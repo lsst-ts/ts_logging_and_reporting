fc9abd1595e8dc395ec8fb494a6aabcf804c7d18
Sebastian Aranda Sanchez <aranda.sebastian@gmail.com>
2025-08-05 18:08:25 -0400

    Add utility functions to tests endpoint authorizations.

--- tests/test_api_endpoints.py
+++ tests/test_api_endpoints.py
@@ -1,7 +1,156 @@
+import os
+import requests
+
+import lsst.ts.logging_and_reporting.utils as ut
+
 from fastapi.testclient import TestClient
 from lsst.ts.logging_and_reporting.web_app.main import app
+from lsst.ts.logging_and_reporting.utils import get_access_token
+from unittest.mock import patch, Mock
 
 client = TestClient(app)
+
+
+SERVICE_ENDPOINT_MOCK_RESPONSES = {
+    "/exposurelog/instruments": {
+        "butler_instruments_1": [
+            "LSSTComCamSim",
+            "LATISS",
+            "LSSTComCam",
+            "LSSTCam",
+        ],
+    },
+    "/exposurelog/exposures": [
+        {
+            "obs_id": "MC_O_20250730_000001",
+            "id": 2025073000001,
+            "instrument": "LSSTCam",
+            "observation_type": "bias",
+            "observation_reason": "bias",
+            "day_obs": 20250730,
+            "seq_num": 1,
+            "group_name": "2025-07-30T20:14:23.653",
+            "target_name": "azel_target",
+            "science_program": "unknown",
+            "tracking_ra": None,
+            "tracking_dec": None,
+            "sky_angle": None,
+            "timespan_begin": "2025-07-30T20:14:23.836969",
+            "timespan_end": "2025-07-30T20:14:23.849000",
+        },
+    ],
+    "/exposurelog/messages": [
+        {
+            "id": "6e915887-0dd0-4335-aa30-fa6a8e61660a",
+            "site_id": "summit",
+            "obs_id": "MC_O_20250730_000001",
+            "instrument": "LSSTCam",
+            "day_obs": 20250730,
+            "seq_num": 1,
+            "message_text": (
+              "Filter change, the M2 haxapod (strut) went  out of position and returned"
+              " to its  previous values\r\n"
+            ),
+            "level": 10,
+            "tags": [
+            "undefined"
+            ],
+            "urls": [],
+            "user_id": "test@localhost",
+            "user_agent": "exposurelog-service",
+            "is_human": True,
+            "is_valid": True,
+            "exposure_flag": "junk",
+            "date_added": "2025-07-30T22:14:23.266086",
+            "date_invalidated": None,
+            "parent_id": None,
+        },
+    ],
+}
+
+def mock_get_response_generator():
+    response_get = requests.Response()
+    response_get.status_code = 200
+    def response_json_payload():
+        called_url = requests.get.call_args[0][0]
+        endpoint = called_url.replace(ut.Server.get_url(), "").split("?")[0]
+        return SERVICE_ENDPOINT_MOCK_RESPONSES[endpoint]
+    response_get.json = response_json_payload
+    while True:
+        yield response_get
+
+def test_endpoint_auth_header(endpoint):
+    headers = {"Authorization": "Bearer header-token"}
+    response = client.get(endpoint, headers=headers)
+    assert response.status_code == 200
+
+def test_endpoint_auth_env_var(endpoint):
+    os.environ["ACCESS_TOKEN"] = "env-token"
+    response = client.get(endpoint)
+    assert response.status_code == 200
+    del os.environ["ACCESS_TOKEN"]
+
+def test_endpoint_auth_rsp_utils(endpoint):
+    mock_lsst = Mock()
+    mock_lsst.rsp.utils.get_info.return_value = "mocked-token"
+
+    with patch.dict("sys.modules", {
+        "lsst": mock_lsst,
+        "lsst.rsp.utils": mock_lsst.rsp.utils,
+    }):
+        response = client.get(endpoint)
+        assert response.status_code == 200
+
+def test_endpoint_no_auth(endpoint):
+    response = client.get(endpoint)
+    assert response.status_code == 401
+
+def test_endpoint_authentication(endpoint):
+    test_endpoint_auth_header(endpoint)
+    test_endpoint_auth_env_var(endpoint)
+    test_endpoint_auth_rsp_utils(endpoint)
+    test_endpoint_no_auth(endpoint)
+
+def test_exposure_entries_endpoint():
+    mock_requests_get_patcher = patch("requests.get")
+    mock_requests_get = mock_requests_get_patcher.start()
+    mock_requests_get.side_effect = mock_get_response_generator()
+
+    endpoint = "/exposure-entries?dayObsStart=20240101&dayObsEnd=20240102&instrument=LSSTCam"
+    test_endpoint_authentication(endpoint)
+
+    app.dependency_overrides[get_access_token] = lambda: "dummy-token"
+    response = client.get(endpoint)
+    assert response.status_code == 200
+    data = response.json()
+    assert "exposure_entries" in data
+    expected_entry_params = [
+        "obs_id",
+        "id",
+        "instrument",
+        "observation_type",
+        "observation_reason",
+        "day_obs",
+        "seq_num",
+        "group_name",
+        "target_name",
+        "science_program",
+        "tracking_ra",
+        "tracking_dec",
+        "sky_angle",
+        "timespan_begin",
+        "timespan_end",
+        "exposure_flag",
+        "exposure_time",
+        "message_text",
+    ]
+    for entry in data["exposure_entries"]:
+        for param in expected_entry_params:
+            assert param in entry, f"Missing {param} in exposure entry: {entry}"
+    app.dependency_overrides.pop(get_access_token, None)
+
+    mock_requests_get_patcher.stop()
+
 
 def test_exposures_endpoint(monkeypatch):
     def mock_get_exposures(dayObsStart, dayObsEnd, instrument, auth_token=None):
@@ -13,9 +162,8 @@ def test_exposures_endpoint(monkeypatch):
         "lsst.ts.logging_and_reporting.web_app.main.get_exposures",
         mock_get_exposures,
     )
-    from lsst.ts.logging_and_reporting.utils import get_access_token
-    app.dependency_overrides[get_access_token] = lambda: "dummy-token"
 
+    app.dependency_overrides[get_access_token] = lambda: "dummy-token"
     response = client.get(
         "/exposures?dayObsStart=20240101&dayObsEnd=20240102&instrument=LSSTCam&auth_token=dummy-token"
     )
@@ -25,7 +173,7 @@ def test_exposures_endpoint(monkeypatch):
     assert data["exposures_count"] == 2
     assert data["sum_exposure_time"] == 30
     assert data["on_sky_exposures_count"] == 1
-    assert data["total_on_sky_exposure_time"] == 10
+    assert data["total_on_sky_exposure_time"] == 30
     app.dependency_overrides.pop(get_access_token, None)
 
 
@@ -53,7 +201,6 @@ def test_exposures_auth_header(monkeypatch):
 
 
 def test_exposures_env_var(monkeypatch):
-    import os
     def mock_get_exposures(dayObsStart, dayObsEnd, instrument, auth_token=None):
         assert auth_token == "env-token"
         return [
