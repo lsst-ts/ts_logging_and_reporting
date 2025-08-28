f4924ce02ac9dd142f74e1e2825bdd10139a26e7
alserene <aserene@swin.edu.au>
2025-08-28 13:23:33 +1000

    Working version of context feed backend.

--- python/lsst/ts/logging_and_reporting/web_app/services/context_feed_service.py
+++ python/lsst/ts/logging_and_reporting/web_app/services/context_feed_service.py
@@ -1,13 +1,10 @@
 import logging
 import os
 from astropy.time import Time, TimeDelta
-import astropy.units as u
-import pandas as pd
 import numpy as np
 
-from rubin_nights import connections
+# from rubin_nights import connections
 from rubin_nights import scriptqueue
-from rubin_nights import scriptqueue_formatting
 import rubin_nights.dayobs_utils as rn_dayobs
 
 from rubin_nights.consdb_query import ConsDbFastAPI, ConsDbTap
@@ -16,32 +13,8 @@ from rubin_nights.logging_query import ExposureLogClient, NarrativeLogClient, Ni
 
 logger = logging.getLogger(__name__)
 
-
-# def get_night_reports(
-#     dayobs_start: int,
-#     dayobs_end: int,
-#     auth_token: str = None
-# ) -> list:
-#     """Get nightreport records for a given time range."""
-#     logger.info(
-#         f"Getting night reports for start: {dayobs_start}, end: {dayobs_end}"
-#     )
-#     nightreport = NightReportAdapter(
-#         server_url=nd_utils.Server.get_url(),
-#         max_dayobs=dayobs_end,
-#         min_dayobs=dayobs_start,
-#         auth_token=auth_token,
-#     )
-#     status = nightreport.get_records()
-#     logger.debug(f"status: {status}")
-#     if status.get('error') is not None:
-#         raise Exception(
-#             f"Error getting nightreport records from"
-#             f" {status.endpoint_url}: {status.error}"
-#         )
-#     return nightreport.records
-
-
+# Modified version of connections.get_clients to allow passing of auth_token
+# Remove and use connections.get_clients once PR merged
 def get_clients(auth_token: str | None = None, site: str | None = None) -> dict:
 
     auth = ("user", auth_token)
@@ -103,7 +76,6 @@ def get_clients(auth_token: str | None = None, site: str | None = None) -> dict:
 
     return endpoints
 
-
 # Required to jasonify pandas DataFrame with special float values
 # To ensure JSON serialisation does not fail
 def stringify_special_floats(val):
@@ -116,7 +88,7 @@ def stringify_special_floats(val):
             return "-Infinity"
     return val
 
-
+# TODO: error handling
 def get_context_feed(
     dayobs_start: int,
     dayobs_end: int,
@@ -126,33 +98,25 @@ def get_context_feed(
     logger.info(
         f"Getting context feed data for start: {dayobs_start}, end: {dayobs_end}"
     )
-
-    print(f"CONTEXT FEED service begins.")
-
-    # Can't use get_clients as is, since auth token is expected to be in a file
+    # Update this to use connections.get_clients once PR merged
     # endpoints = connections.get_clients(tokenfile=tokenfile, site=site)
     endpoints = get_clients(auth_token=auth_token)
 
-    # print(f"CONTEXT FEED endpoints: ", endpoints)
-
     # Convert dayobs_start and dayobs_end to t_start and t_end
     t_start = Time(f"{rn_dayobs.day_obs_int_to_str(dayobs_start)}T12:00:00", format='isot', scale='utc')
-    t_end = Time(f"{rn_dayobs.day_obs_int_to_str(dayobs_end)}T12:00:00", format='isot', scale='utc') + TimeDelta(1, format='jd')
+    t_end = Time(
+                 f"{rn_dayobs.day_obs_int_to_str(dayobs_end)}T12:00:00", format='isot', scale='utc'
+                ) + TimeDelta(1, format='jd')
 
+    # Returns pandas dataframe and list
     df, cols = scriptqueue.get_consolidated_messages(t_start, t_end, endpoints)
-    print(f"TYPE df: ", type(df))
-    print(f"CONTEXT FEED cols: ", cols)
-    print(f"CONTEXT FEED df: ", df)
+
+    # Discard all columns that are not listed in cols
+    df_cols_only = df[df.columns.intersection(cols)]
 
     # Convert special floats (nans and infs) to strings
     # This ensures that JSON serialisation does not fail
-    df_safe = df.map(stringify_special_floats)
-    records = df_safe.to_dict(orient="records") # list!
-    print(f"CONTEXT FEED conversion to dict completed.")
-
-    # print(f"CONTEXT FEED df_to_dict: ", records)
-    print(f"TYPE df_to_dict: ", type(records))
-
-    print(f"CONTEXT FEED service completed.")
+    df_safe = df_cols_only.map(stringify_special_floats)
+    records = df_safe.to_dict(orient="records")
 
     return [records, cols]
